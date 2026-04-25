09fc266c56d6377d08d1a4fe57ec9751c4031a2b
Andres <ambidexteron@gmail.com>
2026-04-25 21:15:38 +0300

    adjust readme

--- dbt/tslots/models/marts/focus/focus__slots_used_monthly.sql
+++ dbt/tslots/models/marts/focus/focus__slots_used_monthly.sql
@@ -3,10 +3,9 @@
 -- slots_used — кол-во уникальных ячеек занятых за месяц.
 
 SELECT
-	MAX(agent_name)                         AS agent_name
-	, MAX(depositor_name)                   AS depositor_name
+	MAX(depositor_name)                   AS depositor_name
 	, TO_CHAR(moment_day, 'YYYY-MM')        AS moment_month
 	, COUNT(DISTINCT slot_id)               AS slots_used
 FROM {{ ref('int_balance__agent_slot_item_daily_spine') }}
-GROUP BY agent_id, depositor_id, TO_CHAR(moment_day, 'YYYY-MM')
-ORDER BY agent_name, depositor_name, moment_month
+GROUP BY depositor_id, TO_CHAR(moment_day, 'YYYY-MM')
+ORDER BY depositor_name, moment_month
